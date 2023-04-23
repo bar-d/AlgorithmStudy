e304379cc112eb54e1ede8d531e983d2eed4047f
minryul <minryul@minryului-Macmini.local>
2023-04-23 17:27:35 +0900

    Problem 2

--- HashTable/Hamu/HashTable_Hamu.playground/Contents.swift
+++ HashTable/Hamu/HashTable_Hamu.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //MARK: - 2085. Count Common Words With One Occurrence
 
-class Solution {
+class Solution1 {
     func addCount(map: [String: Int], words: [String], isSecond: Bool = false) -> [String: Int] {
         var map = map
         words.forEach { word in
@@ -23,5 +23,27 @@ class Solution {
         wordCountMap = wordCountMap.filter { $0.1 == 2 }
         
         return wordCountMap.count
+    }
+}
+
+//MARK: - 2506. Count Pairs Of Similar Strings
+
+class Solution2 {
+
+    func similarPairs(_ words: [String]) -> Int {
+        let words = words.map { Set($0) }
+        var result = 0
+
+        words.enumerated().forEach { index, word in
+            var pairResult = 0
+            for i in (index + 1)..<words.count {
+                if word == words[i] {
+                    pairResult += 1
+                }
+            }
+            result += pairResult
+        }
+        
+        return result
     }
 }
