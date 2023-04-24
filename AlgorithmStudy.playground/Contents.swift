f2e61912fcf0ba2e1d081826876005640c0a69f9
hongssup <hongssup0202@gmail.com>
2023-04-24 19:49:43 +0900

    HashTable 1: 2085. Count Common Words With One Occurrence
    Easy

--- AlgorithmStudy.playground/Contents.swift
+++ AlgorithmStudy.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+//MARK: - 2085. Count Common Words With One Occurrence
+
+class Solution1 {
+    func countWords(_ words1: [String], _ words2: [String]) -> Int {
+        var dict1 = [String:Int]()
+        var dict2 = [String:Int]()
+        var result = 0
+        
+        for word in words1 {
+            dict1[word, default: 0] += 1
+        }
+        
+        for word in words2 {
+            dict2[word, default: 0] += 1
+        }
+        
+        for (key, value) in dict1 {
+            if value == 1 && dict2[key] == 1 {
+                result += 1
+            }
+        }
+        
+        return result
+    }
+}
