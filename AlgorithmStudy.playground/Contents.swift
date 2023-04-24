01e2f6a72275875b58ce95834f4a8ea04e6c0707
hongssup <hongssup0202@gmail.com>
2023-04-24 23:11:58 +0900

    HashTable 3: 2399. Check Distances Between Same Letters

--- AlgorithmStudy.playground/Contents.swift
+++ AlgorithmStudy.playground/Contents.swift
@@ -43,3 +43,29 @@ class Solution2 {
         return result
     }
 }
+
+//MARK: - 2399. Check Distances Between Same Letters
+
+class Solution3 {
+    func checkDistances(_ s: String, _ distance: [Int]) -> Bool {
+        var dict: [Character:Int] = [:]
+        let array = s.map { $0 }
+        
+        for i in 0..<array.count {
+            if let first = dict[array[i]] {
+                dict[array[i]] = i - first - 1
+            } else {
+                dict[array[i]] = i
+            }
+        }
+        
+        for (key, value) in dict {
+            let index = key.asciiValue! - Character("a").asciiValue!
+            if value != distance[Int(index)] {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
