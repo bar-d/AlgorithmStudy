06f7c80e2a5391eed1f29c6451c45c1479d9df1e
dp221125 <dp221125@naver.com>
2023-05-01 15:28:39 +0900

    problem 2 -   Binary Tree Inorder Traversal

--- HashTable/milkyo/AlgorithmStudy/AlgorithmStudyTests/AlgorithmStudyTests_Stack.swift
+++ HashTable/milkyo/AlgorithmStudy/AlgorithmStudyTests/AlgorithmStudyTests_Stack.swift
@@ -25,4 +25,24 @@ class AlgorithmStudyTests_Stack: XCTestCase {
         let result = solution.nextGreaterElement([2,4], [1,2,3,4])
         XCTAssertEqual(result, [3,-1])
     }
+    
+    // MARK: - Binary Tree Inorder Traversal
+    func test_Binary_Tree_Inorder_Traversal_Example1() {
+        let rightLeftNode = TreeNode(3)
+        let rightNode = TreeNode(2, rightLeftNode, nil)
+        let rootNode = TreeNode(1, nil, rightNode)
+        let result = solution.inorderTraversal(rootNode)
+        XCTAssertEqual(result, [1,3,2])
+    }
+    // MARK: - Binary Tree Inorder Traversal
+    func test_Binary_Tree_Inorder_Traversal_Example2() {
+        let result = solution.inorderTraversal(nil)
+        XCTAssertEqual(result, [])
+    }
+    
+    // MARK: - Binary Tree Inorder Traversal
+    func test_Binary_Tree_Inorder_Traversal_Example3() {
+        let result = solution.inorderTraversal(TreeNode(1))
+        XCTAssertEqual(result, [1])
+    }
 }
