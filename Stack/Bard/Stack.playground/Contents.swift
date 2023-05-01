eff7fd355e737673b0e27d7d5b965151130fb496
bard <schy2503@naver.com>
2023-05-01 22:38:04 +0900

    problem 2 - binary tree inorder traversal

--- Stack/Bard/Stack.playground/Contents.swift
+++ Stack/Bard/Stack.playground/Contents.swift
@@ -19,4 +19,47 @@ func nextGreaterElement(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
     return array
 }
 
-nextGreaterElement([4,1,2], [1,3,4,2])
+// MARK: - https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+func inorderTraversal(_ root: TreeNode?) -> [Int] {
+    var currentNode = root
+    var treeNodeArray: [TreeNode] = []
+    var result = [Int]()
+    
+    while !treeNodeArray.isEmpty || currentNode != nil {
+        while currentNode != nil {
+            treeNodeArray.append(currentNode!)
+            currentNode = currentNode?.left
+        }
+        
+        currentNode = treeNodeArray.popLast()
+        result.append(currentNode!.val)
+        currentNode = currentNode?.right
+    }
+    
+    return result
+}
