ea5b97d735902f2a85b766bef04f775bafc6f1b7
Wody95 <kwu0626@gmail.com>
2023-05-05 10:38:05 +0900

    [wody] [5/3] 922. Sort Array By Parity II

--- Sorting/wody/Sortingplayground.playground/Contents.swift
+++ Sorting/wody/Sortingplayground.playground/Contents.swift
@@ -9,7 +9,8 @@ import UIKit
  분할된 숫자의 합이 최소합이 되도록 반환
 
  1. num을 분할하여 array 형태로 변환
- 2.
+ 2. 숫자를 오름차순으로 정렬
+ 3. 순차적으로 num1과 num2로 분배
 
  */
 
@@ -39,3 +40,50 @@ func splitNum(_ num: Int) -> Int {
 }
 
 splitNum(4325)
+
+/*
+ https://leetcode.com/problems/sort-array-by-parity-ii/
+ 922. Sort Array By Parity II
+
+ 1. nums와 동일한 elment count를 가진 [Int] 생성
+ 2. nums를 순회하며 짝수, 홀수 분리
+ 3. nums index를 순회하며 index가 짝수일 경우 짝수 대입, 홀수일 경우 홀수 대입
+
+ */
+
+extension Int {
+    var isEven: Bool {
+        if self % 2 == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+func sortArrayByParityII(_ nums: [Int]) -> [Int] {
+
+    var result = Array(repeating: 0, count: nums.count)
+    var evenNums: [Int] = []
+    var oddNums: [Int] = []
+
+    for num in nums {
+        if num.isEven {
+            evenNums.append(num)
+        } else {
+            oddNums.append(num)
+        }
+    }
+
+    for (index, _) in result.enumerated() {
+        if index.isEven {
+            result[index] = evenNums.popLast() ?? 0
+        } else {
+            result[index] = oddNums.popLast() ?? 0
+        }
+    }
+
+    return result
+}
+
+print(sortArrayByParityII([4,2,5,7]))
