04fa9c261de9659460eb275e30c2073fe7a76b66
minryul <rud0806@gmail.com>
2023-05-07 21:07:20 +0900

    Problem 5

--- Sorting/Hamu/Sorting_Hamu.playground/Contents.swift
+++ Sorting/Hamu/Sorting_Hamu.playground/Contents.swift
@@ -62,19 +62,40 @@ class Solution4 {
             }[k - 1].0
     }
 }
-func getKth(_ lo: Int, _ hi: Int, _ k: Int) -> Int {
-    return Array(lo...hi)
-        .map { num -> (Int, Int) in
-            var temp = num, count = 0
-            while temp != 1 {
+
+//MARK: - 969. Pancake Sorting
+
+class Solution5 {
+    func pancakeSort(_ arr: [Int]) -> [Int] {
+        let answer = arr.sorted()
+        var sortedArr = answer
+        var arr = arr, result = [Int](), swap = arr.count
+        if arr == sortedArr { return result }
+        
+        while arr != answer {
+            var temp = [Int](), last = [Int](), isRevers = false, count = 0
+            arr.forEach {
                 count += 1
-                if temp % 2 == 0 { temp /= 2 }
-                else { temp = 3 * temp + 1 }
+                if isRevers {
+                    last.append($0)
+                    return
+                }
+                temp.append($0)
+                if sortedArr.last == $0 {
+                    sortedArr.popLast()
+                    isRevers = true
+                    temp.reverse()
+                    result.append(count)
+                }
             }
-            return (num, count)
+            
+            arr = [temp, last].flatMap { $0 }
+            var swapArray = arr.prefix(swap)
+            let lastArray = arr.suffix(arr.count - swap)
+            arr = Array(swapArray.reversed()) + lastArray
+            result.append(swap)
+            swap -= 1
         }
-        .sorted {
-            if $0.1 == $1.1 { return $0.0 < $1.0 }
-            return $0.1 < $1.1
-        }[k - 1].0
+        return result
+    }
 }
