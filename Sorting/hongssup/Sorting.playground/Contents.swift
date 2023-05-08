5a71a05988002a02d05d2073577fad01c10237b9
hongssup <hongssup0202@gmail.com>
2023-05-08 23:59:49 +0900

    problem 3 - intersection of two arrays

--- Sorting/hongssup/Sorting.playground/Contents.swift
+++ Sorting/hongssup/Sorting.playground/Contents.swift
@@ -16,3 +16,27 @@ class Solution1 {
         return Int(num1)! + Int(num2)!
     }
 }
+//MARK: - 349. Intersection of Two Arrays
+
+class Solution3_1 {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        var dict = [Int:Bool]()
+        for i in nums1 {
+            dict[i] = true
+        }
+        var result = [Int]()
+        for i in Set(nums2) {
+            if let _ = dict[i] {
+                result.append(i)
+            }
+        }
+        return result
+    }
+}
+
+class Solution3_2 {
+    func intersection(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+        return Array(Set(nums1).intersection(nums2))
+    }
+}
+
