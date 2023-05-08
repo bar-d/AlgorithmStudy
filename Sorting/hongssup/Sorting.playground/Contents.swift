07876cab943f3e10cfcffa4d1803f0bc2b005ade
hongssup <hongssup0202@gmail.com>
2023-05-09 00:00:53 +0900

    problem 4 - sort integers by the power value

--- Sorting/hongssup/Sorting.playground/Contents.swift
+++ Sorting/hongssup/Sorting.playground/Contents.swift
@@ -40,3 +40,51 @@ class Solution3_2 {
     }
 }
 
+//MARK: - 1387. Sort Integers by The Power Value
+
+class Solution4_1 {
+    func getKth(_ lo: Int, _ hi: Int, _ k: Int) -> Int {
+        var dict = [Int:Int]()
+        for i in lo...hi {
+            dict[i] = getPower(i)
+        }
+        let result = dict.sorted(by: { ($0.value, $0.key) < ($1.value, $1.key) })
+
+        return result[k-1].key
+    }
+
+    func getPower(_ x: Int) -> Int {
+        var x = x
+        var power = 0
+        while x != 1 {
+            x = (x % 2 == 0) ? x / 2 : 3 * x + 1
+            power += 1
+        }
+        return power
+    }
+}
+
+class Solution4_2 {
+    var dp: [Int:Int] = [1:0]
+
+    func getKth(_ lo: Int, _ hi: Int, _ k: Int) -> Int {
+        var dict = [Int:Int]()
+        for i in lo...hi {
+            dict[i] = getPower(i)
+        }
+        let result = dict.sorted(by: { ($0.value, $0.key) < ($1.value, $1.key) })
+
+        return result[k-1].key
+    }
+
+    func getPower(_ x: Int) -> Int {
+        if let power = dp[x] {
+            return power
+        }
+        let power = (x % 2 == 0) ? getPower(x/2) + 1 : getPower(3 * x + 1) + 1
+        dp[x] = power
+
+        return power
+    }
+}
+
