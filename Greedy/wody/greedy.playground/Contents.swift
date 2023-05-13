c26a15d9e0c0245d3a223102344f6df214281888
Wody95 <kwu0626@gmail.com>
2023-05-13 10:07:27 +0900

    [wody] 2번째 문제 - 2224. Minimum Number of Operations to Convert Time

--- Greedy/wody/greedy.playground/Contents.swift
+++ Greedy/wody/greedy.playground/Contents.swift
@@ -51,3 +51,64 @@ func maxDistance(_ colors: [Int]) -> Int {
 }
 
 print(maxDistance([1,8,3,8,3]))
+
+
+/*
+ https://leetcode.com/problems/minimum-number-of-operations-to-convert-time/
+ 2224. Minimum Number of Operations to Convert Time
+ 시간 변환을 위한 최소 작업 수
+
+ current와 correct에 전달받는 시간은 24시간 표기법을 따릅니다.
+ 시간 변환에 증가하는 시간의 단위는 1, 5, 15, 60분 입니다.
+ 현재부터 목표 시간까지 시간 변환한이 이뤄진다면, 변환이 이뤄진 최소 작업 수를 반환하면 됩니다.
+
+ ex) current = "02:30" -> correct = "04:35"
+ output : 3
+ 1. Add 60분 : "02:30" -> "03:30"
+ 2. Add 60분 : "03:30" -> "04:30"
+ 3. Add 5분 : "04:30" -> "04:35"
+ 총 작업횟수 3회
+
+ 문제풀이 설계
+ - String으로 들어오는 시간을 TimeInterval(Double)로 변환하면 쉽게 풀이할 수 있을 것 같다.
+ - 시간 변환에 증가하는 단위를 정책(Policy)로 정의해 매직넘버를 줄여보자.
+
+ 1. String -> TimeInterval 변환
+ 2. 남은 분 산출 correct - current / 60
+ 2. 남은 분에서 각 작업 단위 실행
+ */
+enum Policy {
+    static let oneMinuteSecond = 60
+    static let oneMinute = 1
+    static let fiveMinute = 5
+    static let fifteenMinute = 15
+    static let sixtyMinute = 60
+}
+
+extension String {
+    var toTimeInterval: TimeInterval {
+        guard self != "" else { return 0 }
+
+        var timeInterval: Double = 0
+
+        var separatedTime = self.components(separatedBy: ":")
+
+        for (index, time) in separatedTime.reversed().enumerated() {
+            timeInterval += (Double(time) ?? 0) * pow(Double(60), Double(index+1))
+        }
+
+        return timeInterval
+    }
+}
+
+func convertTime(_ current: String, _ correct: String) -> Int {
+    let remainingTime = Int(correct.toTimeInterval - current.toTimeInterval) / Policy.oneMinuteSecond
+    let sixtyMinuteWorkNumber = remainingTime / Policy.sixtyMinute
+    let fifteenMinuteWorkNumber = (remainingTime % Policy.sixtyMinute) / Policy.fifteenMinute
+    let fiveMineteWorkNumber = ((remainingTime % Policy.sixtyMinute) % Policy.fifteenMinute) / Policy.fiveMinute
+    let oneMineteWorkNumber = (((remainingTime % Policy.sixtyMinute) % Policy.fifteenMinute) % Policy.fiveMinute) / Policy.oneMinute
+
+    return sixtyMinuteWorkNumber + fifteenMinuteWorkNumber + fiveMineteWorkNumber + oneMineteWorkNumber
+}
+
+print(convertTime("02:30", "04:35"))
