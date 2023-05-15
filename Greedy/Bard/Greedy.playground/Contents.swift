45669e9b1cf94bac73503ab24516cd65d3a7a7b2
bard <schy2503@naver.com>
2023-05-15 09:52:47 +0900

    problem 2 - minimum number of operations to convert time

--- Greedy/Bard/Greedy.playground/Contents.swift
+++ Greedy/Bard/Greedy.playground/Contents.swift
@@ -15,3 +15,42 @@ func maxDistance(_ colors: [Int]) -> Int {
     
     return max(firstResult - 0, secondResult - currentIndex)
 }
+
+// MARK: - https://leetcode.com/problems/minimum-number-of-operations-to-convert-time/
+
+func convertTime(_ current: String, _ correct: String) -> Int {
+    var count = 0
+    var currentHour = Int(current.split(separator: ":").first!)!
+    var currentMinute = Int(current.split(separator: ":").last!)!
+    let correctHour = Int(correct.split(separator: ":").first!)!
+    let correctMinute = Int(correct.split(separator: ":").last!)!
+    
+    while currentMinute != correctMinute {
+        if (correctMinute - currentMinute) % 15 == 0 {
+            currentMinute += 15
+            count += 1
+        } else if (correctMinute - currentMinute) % 5 == 0 {
+            currentMinute += 5
+            count += 1
+        } else {
+            currentMinute += 1
+            count += 1
+        }
+        
+        if currentMinute >= 60 {
+            currentMinute -= 60
+            currentHour += 1
+        }
+    }
+    
+    while currentHour != correctHour {
+        currentHour += 1
+        count += 1
+        
+        if currentHour == 24 {
+            currentHour = 0
+        }
+    }
+    
+    return count
+}
