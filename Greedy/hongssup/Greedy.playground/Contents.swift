6d19a6e2376ef0758ed65111cd94c2d5effdeafe
hongssup <hongssup0202@gmail.com>
2023-05-16 22:20:33 +0900

    problem 2 - Minimum Number of Operations to Convert Time

--- Greedy/hongssup/Greedy.playground/Contents.swift
+++ Greedy/hongssup/Greedy.playground/Contents.swift
@@ -16,3 +16,24 @@ class Solution1 {
         return result
     }
 }
+
+//MARK: - 2224. Minimum Number of Operations to Convert Time
+
+class Solution2 {
+    func convertTime(_ current: String, _ correct: String) -> Int {
+        let current = (Int(current.prefix(2))! * 60) + Int(current.suffix(2))!
+        let correct = (Int(correct.prefix(2))! * 60) + Int(correct.suffix(2))!
+        var interval = correct - current
+
+        var result = 0
+        let operationTime = [60,15,5,1]
+        
+        for i in operationTime {
+            result += interval / i
+            interval = interval % i
+            if interval == 0 { break }
+        }
+
+        return result
+    }
+}
