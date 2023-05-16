6008b0b7e446e25aee7489bea12838ab5d1107d3
minryul <rud0806@gmail.com>
2023-05-16 20:56:18 +0900

    Problem 2

--- Greedy/hamu/Greedy.playground/Contents.swift
+++ Greedy/hamu/Greedy.playground/Contents.swift
@@ -13,3 +13,30 @@ class Solution1 {
             .max() ?? 0
     }
 }
+
+//2224. Minimum Number of Operations to Convert Time
+class Solution2 {
+    func convertTime(_ current: String, _ correct: String) -> Int {
+        var current = current.split(separator: ":").map { Int($0)! }.reduce(0) { $0 * 60 + $1 }
+        let correct = correct.split(separator: ":").map { Int($0)! }.reduce(0) { $0 * 60 + $1 }
+        let timeList = [1, 5, 15, 60]
+        var count = 0
+        
+        while current != correct {
+            count += 1
+            var addTime = 1
+            for (index, time) in timeList.enumerated() {
+                addTime = time
+                if current + time > correct {
+                    addTime = timeList[index - 1]
+                    break
+                } else if current + time == correct {
+                    return count
+                }
+            }
+            current += addTime
+        }
+        
+        return count
+    }
+}
