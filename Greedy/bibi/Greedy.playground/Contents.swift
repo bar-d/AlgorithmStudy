048898de7f571f3295798cfc2bfa0dadb87377d4
doyeonjeong <debby_@kakao.com>
2023-05-17 12:47:23 +0900

    problem 2 - 2224. Minimum Number of Operations to Convert Time

--- Greedy/bibi/Greedy.playground/Contents.swift
+++ Greedy/bibi/Greedy.playground/Contents.swift
@@ -18,3 +18,32 @@ class Solution1 {
         return max(right, last - left)
     }
 }
+
+// problem 2 - 2224. Minimum Number of Operations to Convert Time
+class Solution2 {
+    func convertTime(_ current: String, _ correct: String) -> Int {
+        
+        func getTotalMinutes(_ times: [Int]) -> Int {
+            let hours = times[0]
+            let minutes = times[1]
+            return hours * 60 + minutes
+        }
+        
+        let currentMinutes = getTotalMinutes(current.split(separator: ":").map { Int($0)! })
+        let correctMinutes = getTotalMinutes(correct.split(separator: ":").map { Int($0)! })
+        
+        var diff = correctMinutes - currentMinutes
+        
+        var result = 0
+        let minutes = [60, 15, 5, 1]
+        
+        for minute in minutes {
+            // 현재 분 단위로 나눈 횟수를 결과에 더함
+            result += diff / minute
+            // 현재 분 단위로 나눈 나머지를 다음 계산을 위해 저장
+            diff %= minute
+        }
+        
+        return result
+    }
+}
