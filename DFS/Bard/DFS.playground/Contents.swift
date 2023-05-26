abebdfa64fd856af33bd1fb280b60faf6ecab9e1
bard <schy2503@naver.com>
2023-05-26 14:06:32 +0900

    problem 2 - island perimeter

--- DFS/Bard/DFS.playground/Contents.swift
+++ DFS/Bard/DFS.playground/Contents.swift
@@ -29,19 +29,59 @@ class Solution {
         guard let root = root else {
             return true
         }
-
+        
         return checkValue(of: root, root.val)
     }
-
+    
     func checkValue(of node: TreeNode?, _ value: Int) -> Bool {
         guard let node = node else {
             return true
         }
-
+        
         if node.val != value {
             return false
         }
-
+        
         return checkValue(of: node.left, value) && checkValue(of: node.right, value)
     }
+}
+
+// MARK: - https://leetcode.com/problems/island-perimeter/description/
+
+func islandPerimeter(_ grid: [[Int]]) -> Int {
+    var count = 0
+    
+    for row in 0..<grid.count {
+        for column in 0..<grid[row].count {
+            let currentCell = grid[row][column]
+            
+            if currentCell == 1 {
+                if row == 0 || row == grid.count - 1 {
+                    count += 1
+                }
+                
+                if column == 0 || column == grid[row].count - 1 {
+                    count += 1
+                }
+                
+                if column - 1 >= 0 && grid[row][column - 1] == 0 {
+                    count += 1
+                }
+                
+                if column < grid[row].count - 1 && grid[row][column + 1] == 0 {
+                    count += 1
+                }
+                
+                if row - 1 >= 0 && grid[row - 1][column] == 0 {
+                    count += 1
+                }
+                
+                if row < grid.count - 1 && grid[row + 1][column] == 0 {
+                    count += 1
+                }
+            }
+        }
+    }
+    
+    return count
 }
