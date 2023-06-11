37272a30c22b8c3aad7547f9aca03bb8326d4551
bard <schy2503@naver.com>
2023-06-12 00:24:54 +0900

    problem 2 - average of levels in binary tree

--- BFS/Bard/BFS.playground/Contents.swift
+++ BFS/Bard/BFS.playground/Contents.swift
@@ -32,3 +32,60 @@ func maxDepth(_ root: Node?) -> Int {
     
     return depth + 1
 }
+
+// MARK: -  https://leetcode.com/problems/average-of-levels-in-binary-tree/
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public init() {
+        self.val = 0
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+    
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+func averageOfLevels(_ root: TreeNode?) -> [Double] {
+    guard let root = root else {
+        return []
+    }
+    
+    var nodes = [root]
+    var result: [Double] = []
+    
+    while !nodes.isEmpty {
+        var total: Double = 0.0
+        var newNodes: [TreeNode?] = []
+        var count: Double = 0
+        
+        for node in nodes {
+            count += 1
+            total += Double(node.val)
+        }
+        
+        result.append(total / count)
+        
+        for node in nodes {
+            newNodes.append(node.left)
+            newNodes.append(node.right)
+        }
+        
+        nodes = newNodes.compactMap { $0 }
+    }
+    
+    return result
+}
