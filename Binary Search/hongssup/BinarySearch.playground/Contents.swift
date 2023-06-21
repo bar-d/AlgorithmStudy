2e06bedbfc96e3a4b1759044dbd27a498e38203b
hongssup <hongssup0202@gmail.com>
2023-06-21 22:49:29 +0900

    problem 2 - longest subsequence with limited sum

--- Binary Search/hongssup/BinarySearch.playground/Contents.swift
+++ Binary Search/hongssup/BinarySearch.playground/Contents.swift
@@ -27,3 +27,36 @@ func binarySearch(_ arr: [Int]) -> Int {
     }
     return start
 }
+
+// MARK: - https://leetcode.com/problems/longest-subsequence-with-limited-sum/description/
+
+func answerQueries(_ nums: [Int], _ queries: [Int]) -> [Int] {
+    var nums = nums.sorted()
+    var sums = Array(repeating: 0, count: nums.count + 1)
+    var result = [Int]()
+    
+    for i in 1...nums.count {
+        sums[i] = sums[i-1] + nums[i-1]
+    }
+    
+    for i in queries {
+        result.append(binarySearch(sums, i))
+    }
+    
+    return result
+}
+
+func binarySearch(_ sums: [Int], _ query: Int) -> Int {
+    var s = 0
+    var e = sums.count
+    
+    while s < e {
+        let mid = (s + e) / 2
+        if sums[mid] <= query {
+            s = mid + 1
+        } else {
+            e = mid
+        }
+    }
+    return s - 1
+}
