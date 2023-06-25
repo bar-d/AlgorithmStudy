7088a1829a2b59b9f86905c7652801e95837f7aa
bard <schy2503@naver.com>
2023-06-26 01:47:23 +0900

    problem 3 - find the town judge

--- Graph/Bard/Graph.playground/Contents.swift
+++ Graph/Bard/Graph.playground/Contents.swift
@@ -9,5 +9,25 @@ func findCenter(_ edges: [[Int]]) -> Int {
         return 0
     }
     
-    return Set(first).intersection(last).first!
+    return Set(first).intersection(last).first ?? 0
+}
+
+// MARK: - https://leetcode.com/problems/find-the-town-judge/description/
+
+func findJudge(_ n: Int, _ trust: [[Int]]) -> Int {
+    var trusts = Array(repeating: 0, count: n + 1)
+    var trusted = Array(repeating: 0, count: n + 1)
+    
+    for t in trust {
+        trusts[t[0]] += 1
+        trusted[t[1]] += 1
+    }
+    
+    for i in 1...n {
+        if trusts[i] == 0 && trusted[i] == n - 1 {
+            return i
+        }
+    }
+    
+    return -1
 }
