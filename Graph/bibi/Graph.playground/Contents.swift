c9d20978e22e9dcf34fe3d9c239a6a9c6e4cb6e1
doyeonjeong <debby_@kakao.com>
2023-06-28 17:10:59 +0900

    problem 3 - find the town judge

--- Graph/bibi/Graph.playground/Contents.swift
+++ Graph/bibi/Graph.playground/Contents.swift
@@ -11,3 +11,24 @@ func findCenter(_ edges: [[Int]]) -> Int {
     return -1
 }
 
+// MARK: - https://leetcode.com/problems/find-the-town-judge
+
+func findJudge(_ n: Int, _ trust: [[Int]]) -> Int {
+    var trustOthers = Array(repeating: 0, count: n + 1)
+    var isTrusted = Array(repeating: 0, count: n + 1)
+
+    for pair in trust {
+        let truster = pair[0]
+        let trustee = pair[1]
+        trustOthers[truster] += 1
+        isTrusted[trustee] += 1
+    }
+
+    for i in 1...n {
+        if trustOthers[i] == 0 && isTrusted[i] == n - 1 {
+            return i
+        }
+    }
+
+    return -1
+}
